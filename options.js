9f1ad8ebb4d5fd6a8082c67b37058777fe7bf283
Jelle Verzijden <Jelle@macaw.nl>
2016-06-07 11:46:15 +0200

    CHANGED Default port to 8888 to prevent obstruction when running more then one instance + ADD custom template for specs

--- options.js
+++ options.js
@@ -1,32 +1,42 @@
 // User options for overriding core options.js
 
 module.exports = {
-    // Restart the app after changing core (back-end) options
-    // Core options could be only redefined from user/options.js, context options are not supported
-    core: {
+  // Restart the app after changing core (back-end) options
+  // Core options could be only redefined from user/options.js, context options are not supported
+  core: {
+    server: {
+      port: 8888,
+    },
+  },
+
+  // Page rendering configuration (redefinable from context options)
+  rendering: {
+    // Define views for rendering SourceJS pages (array order define priority)
+    views: {
+      spec: [
+      '$(user)/core/views/spec_global.ejs',
+      ],
+    },
+  },
+
+  // Client-side options (redefinable from context options)
+  assets: {
+    modulesEnabled: {
+        // Overriding example
+        // trimSpaces: true
     },
 
-    // Page rendering configuration (redefinable from context options)
-    rendering: {},
-
-    // Client-side options (redefinable from context options)
-    assets: {
-        modulesEnabled : {
-            // Overriding example
-            // trimSpaces: true
-        },
-
-        modulesOptions : {
-            // Modules options example
-            // navHighlight: {
-            //     updateHash: false
-            // }
-        },
-        
-        // Legacy options object support for some older plugins
-        pluginsOptions: {}
+    modulesOptions: {
+        // Modules options example
+        // navHighlight: {
+        //     updateHash: false
+        // }
     },
 
-    // External plugins options (are also exposed to client-side
-    plugins: {}
+    // Legacy options object support for some older plugins
+    pluginsOptions: {},
+  },
+
+  // External plugins options (are also exposed to client-side
+  plugins: {},
 };
