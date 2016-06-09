47fa91db82cad95f7d95faf8cd2f9d891b516734
Jelle Verzijden <Jelle@macaw.nl>
2016-06-09 13:10:49 +0200

    ADDED Gulp task to check for files named sourcejs.scss and compile them + ADDED sourcejs example css link to template

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var sassCompiler = require('gulp-sass');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 
-var scssGlob = './specs/**/*.scss';
+var scssGlob = ['./specs/**/*.scss', '!./specs/**/*sourcejs.scss']; //Find files with scss extension but ignore files that are named sourcejs.scss
+var sourcejsScssGlob = './specs/**/*sourcejs.scss'; //Find files named sourcejs.scss
 var jsGlob = './specs/**/*.js';
 
 gulp.task('sassCompiler', function () {
@@ -15,10 +16,11 @@ gulp.task('sassCompiler', function () {
   .pipe(gulp.dest('./dist/css/')); //Set destination folder
 });
 
-gulp.task('jsMinifier', function () {
-  return gulp.src(jsGlob) //Find all files with .scss extension recursively starting from rootfolder
-	.pipe(concat('main.js')) //Concat the files to one file
-  .pipe(gulp.dest('./dist/js/')); //Set destination folder
+gulp.task('sourceJsSassCompiler', function () {
+  return gulp.src(sourcejsScssGlob) //Find all files named sourcejs.scss
+	.pipe(sassCompiler({ outputStyle: 'compressed' }).on('error', sassCompiler.logError)) //Compile using sassCompiler
+	.pipe(concat('sourcejs.min.css')) //Concat the files to one file
+  .pipe(gulp.dest('./dist/css/')); //Set destination folder
 });
 
 gulp.task('jsMinifier', function () {
@@ -36,7 +38,8 @@ gulp.task('jsMinifier', function () {
     .pipe(gulp.dest('dist/js/'));
 });
 
-gulp.task('watch', ['sassCompiler', 'jsMinifier'], function () {
+gulp.task('watch', ['sassCompiler', 'sourceJsSassCompiler', 'jsMinifier'], function () {
   gulp.watch(scssGlob, ['sassCompiler']);
+  gulp.watch(sourcejsScssGlob, ['sourceJsSassCompiler']);
   gulp.watch(jsGlob, ['jsMinifier']);
 });
